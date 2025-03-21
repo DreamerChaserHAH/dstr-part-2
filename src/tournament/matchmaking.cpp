484d4e37205664769eb8c69acbee516773e8de30
Victor Mak <contact@htetaung.com>
2025-03-21 20:20:05 +0800

    feat: apu tennis tournament application

--- src/tournament/matchmaking.cpp
+++ src/tournament/matchmaking.cpp
@@ -1,5 +1,104 @@
 #include <tournament/matchmaking.hpp>
 
+QualifierRoundMatchmakingSystem::QualifierRoundMatchmakingSystem(Player** player_list, int number_of_qualifying_players) : BaseMatchmakingSystem(QUALIFIER, player_list){
+    for(int i = 0; i < number_of_qualifying_players; i++){
+        int current_index = i % 2 == 0 ? (i == 0? 0: i/2) : (number_of_qualifying_players - 1) - (i/2);
+        player_list[current_index]->performance.current_round = MATCH_TYPE::QUALIFIER;
+    }
+    this->matchmaking_queue = new PlayerDoubleEndedPriorityQueue(this->all_players, number_of_qualifying_players);
+    this->number_of_total_qualifying_players = number_of_qualifying_players;
+    this->number_of_remaining_players = number_of_qualifying_players;
+}
+
+MatchesContainer* QualifierRoundMatchmakingSystem::matchmake() {
+    int potential_remaining_players_after_matchmaking = this->number_of_remaining_players;
+    int potential_players_in_queue = this->matchmaking_queue->number_of_remaining_players_in_queue;
+    if (potential_remaining_players_after_matchmaking == 8 && potential_players_in_queue == 8) {
+        // the process of getting players is now completed, the other tournament class move to round robin stage
+        set_is_completed(true);
+        return nullptr;
+    }
+
+    int number_of_matches_to_be_made = 0;
+    while (potential_remaining_players_after_matchmaking > 8 && potential_players_in_queue > 0) {
+        number_of_matches_to_be_made++;
+        potential_players_in_queue -= 2;
+        potential_remaining_players_after_matchmaking -= 1;
+    }
+
+    auto* matches_container = new MatchesContainer;
+    matches_container->matches = new Match[number_of_matches_to_be_made];
+    for (int i = 0; i < number_of_matches_to_be_made; i++) {
+        Match new_match{};
+        new_match.createMatch(QUALIFIER, matchmaking_queue->get_min(), matchmaking_queue->get_max());
+        matchmaking_queue->dequeue_min();
+        matchmaking_queue->dequeue_max();
+        matches_container->matches[i] = new_match;
+    }
+
+    std::cout << "Number of Remaining Players : " << this->number_of_remaining_players << std::endl;;
+    std::cout << "Matches Created : " << number_of_matches_to_be_made << std::endl;
+
+    this->number_of_remaining_players = potential_remaining_players_after_matchmaking;
+    matches_container->number_of_matches = number_of_matches_to_be_made;
+    return matches_container;
+}
+
+KnockoutRoundMatchmakingSystem::KnockoutRoundMatchmakingSystem(Player** all_players) : BaseMatchmakingSystem(MATCH_TYPE::KNOCKOUT, all_players) {
+    knockout_round_queue = new PlayerDoubleEndedPriorityQueue(all_players, 32);
+    this->number_of_remaining_players = 32;
+    player_ranking = new PlayerRanking(32);
+    for (int i = 0; i < 32; i++) {
+        all_players[i]->performance.current_round = MATCH_TYPE::KNOCKOUT;
+    }
+}
+
+MatchesContainer* KnockoutRoundMatchmakingSystem::matchmake() {
+    //number of remaining players that will move onto the next stage
+    int potential_remaining_players_after_matchmaking = this->number_of_remaining_players;
+
+    //number of players that currently available to be matchmade
+    int potential_players_in_queue = this->knockout_round_queue->number_of_remaining_players_in_queue;
+
+    if (potential_remaining_players_after_matchmaking == 1) {
+        // the process of getting players is now completed, the other tournament class move to round robin stage
+        set_is_completed(true);
+        push_to_ranking(knockout_round_queue->get_max());
+        return nullptr;
+    }
+
+    int number_of_matches_to_be_made = 0;
+    while (potential_players_in_queue > 0 && potential_remaining_players_after_matchmaking > 0) {
+        number_of_matches_to_be_made++;
+        potential_players_in_queue -= 2;
+        potential_remaining_players_after_matchmaking -= 1;
+    }
+
+    if (number_of_matches_to_be_made == 0) {
+        return nullptr;
+    }
+
+    auto* matches_container = new MatchesContainer;
+    matches_container->matches = new Match[number_of_matches_to_be_made];
+    for (int i = 0; i < number_of_matches_to_be_made; i++) {
+        Match new_match{};
+        Player* player1_info = this->knockout_round_queue->get_min();
+        this->knockout_round_queue->dequeue_min();
+        Player* player2_info = this->knockout_round_queue->get_max();
+        this->knockout_round_queue->dequeue_max();
+        new_match.createMatch(KNOCKOUT, player1_info, player2_info);
+        std::cout << player1_info->name << " " << player2_info->name << std::endl;
+        matches_container->matches[i] = new_match;
+    }
+
+    std::cout << "Number of Remaining Players : " << potential_remaining_players_after_matchmaking << std::endl;;
+    std::cout << "Matches Created : " << number_of_matches_to_be_made << std::endl;
+
+    this->number_of_remaining_players = potential_remaining_players_after_matchmaking;
+    matches_container->number_of_matches = number_of_matches_to_be_made;
+    return matches_container;
+}
+
 MatchmakingSystem::MatchmakingSystem(Player** player_list, int number_of_players, TournamentSchedulingSystem* tss){
     this->player_list = new Player*[number_of_players];
 
